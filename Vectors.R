33e7f570d57c15a63636857b549f8fc6e73183ff
MarilynDCosta <125499347+MarilynDCosta@users.noreply.github.com>
2024-08-19 23:28:20 +0100

    Update Vectors.R

--- Vectors.R
+++ Vectors.R
@@ -58,7 +58,10 @@ decimals[c(1,2)] <- c(7.5, 9.5) # the first two elements are now 7.5 and 9.5 ins
 decimals
 
 # To repeat vectors use rep().
-# rep() involves the elements of the vector being repeated a number of times before the next element is repeated.
+# rep() involves the elements of the vector being repeated a number of time before the next element is repeated.
+# - to repeat each value use each
+# - to repeat a sequence use times
+# - to repeat each value independently create a vector for time, i.e.: times = c(m,...,n)
 
 repeat_integers <- rep(integers, each=3)
 repeat_integers
@@ -68,3 +71,41 @@ repeat_some_integers
 
 repeat_some_decimals <- rep(decimals[c(2,3,4)], each=4)
 repeat_some_decimals
+
+repeat_sequence <- rep(c(9,8,7), times=3)
+repeat_sequence
+
+repeat_values_independently <- rep(c(5,6,7,8), times=c(1,2,3,4))
+repeat_values_independently
+
+# To generate sequenced vectors, you can use : operator
+# - see the vector integers as an example
+# - to make bigger or smaller steps you can use the seq() function
+
+multiples_of_10 <- seq(from=0, to=120, by=10)
+multiples_of_10
+
+# Note: you can combine vectors together 
+# - However, be warned that if the vectors contain elements with different data types, the combined vector created would involve one vector's elements' data type changing to the other vector's data type.
+# - This is also good if you want to remove elements between both lists if you apply the syntax rules we have learnt in this tutorial
+
+a <- c(1,2,3)
+b <- c(1,2,3,4,5,6,20)
+
+c <- c(a,b)
+c
+
+d <- c(b,-a) # -a in this case is the vector a's elements becoming negative.
+d # - Note: -2*a would result in a's elements being multiplied by -2.
+
+e <- c(b[-a]) # removes the elements that are common in both b and a
+e
+
+f <- c(b[c(-a,-7)])
+f
+
+g <- c(b,days) # b's elements have become strings so that b and days can be concaternated to form vector g
+g # hence vector g contains all string elements
+
+h <- c(b, logical_values) # b's elements have become integers (FALSE being 0 and TRUE being 1) so that b and logical_values can be concaternated to form vector g
+h # hence vector h contains all integer elements
