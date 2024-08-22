f8d63c454e85da4ecc50292e9c3c0580052532bb
MarilynDCosta <125499347+MarilynDCosta@users.noreply.github.com>
2024-08-22 22:10:11 +0100

    Update Matrices.R

--- Matrices.R
+++ Matrices.R
@@ -17,3 +17,24 @@ colours_matrix[2,1] # "green"
 # The whole row can be accessed if you leave the rowNumber blank in the index.
 # The whole column can be accessed if you leave the colNumber blank in the index.
 
+numbers_matrix[2,] # specifies 2nd row of numbers_matrix; 3 and 4 would be printed.
+colours_matrix[,2] # specifies 2nd column of colours_matrix; orange and blue would be printed.
+
+# To access multiple columns and/or rows you can use the c() within the index.
+
+numbers_matrix[c(2,3),] # displays the values in the 2nd and 3rd rows of the numbers_matrix: 3, 4, 5 and 6.
+colours_matrix[, c(1,3)] # displays the values in the 1st and 3rd columns of the colours_matrix: "red", "yellow", "green" and "purple"
+
+countries_matrix <- matrix(c("Albania","Denmark","Greece","Belgium","Egypt","Hungary","Canada","Finland","Iceland"), nrow = 3, ncol = 3)
+countries_matrix
+
+countries_matrix[c(2,3), c(1,3)] # Specifies items in the 2nd or 3rd row and 1st or 3rd column of countries_matrix: "Denmark", "Finland", "Greece", "Iceland"
+
+# To add columns to a matrix use cbind().
+# To add rows to a matrix use rbind().
+
+countries_matrix <- cbind(countries_matrix, c("Colombia","France","Iraq"))
+countries_matrix
+
+countries_matrix <- rbind(countries_matrix, c("Japan","Kenya","Libya","Morocco"))
+countries_matrix
