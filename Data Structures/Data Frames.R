58a1ec415361aba078ee2659a10773b335239f72
MarilynDCosta <125499347+MarilynDCosta@users.noreply.github.com>
2024-09-11 23:57:06 +0100

    Add files via upload

---
+++ Data Structures/Data Frames.R
@@ -0,0 +1,15 @@
+# Data frames are data displayed in a table format.
+# Data frames can have different data types within them but each column of data must have the same type of data.
+# Use data.frame() to create a data frame.
+
+Person_Info <- data.frame(
+  Name = c("Marilyn", "John", "Jane"),
+  Age = c(21, 67, 34),
+  Height = c(147, 180, 162)
+)
+
+Person_Info
+
+# Use summary() to summarise data in a data frame.
+
+summary(Person_Info)
