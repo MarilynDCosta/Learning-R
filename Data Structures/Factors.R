2c447d70e70b07328d9c45d00335ae5354041839
MarilynDCosta <125499347+MarilynDCosta@users.noreply.github.com>
2024-09-17 23:58:56 +0100

    Add files via upload

---
+++ Data Structures/Factors.R
@@ -0,0 +1,34 @@
+# Factors are used to categorise data. 
+# To create a factor, use the factor() function and add a vector as an argument.
+
+film_genre <- factor(c("Romance", "Comedy", "Drama", "Thriller", "Horror", "Mystery", "Musical", "Musical", "Crime", "Drama", "Romance"))
+
+film_genre
+
+# To only preint the levels, use the levels() function.
+
+levels(film_genre)
+
+# Instead of using levels(), you ould add a levels argument to factor().
+# This is useful when you have data that does not have all the categories in it as you have more types of categories than the number of categories within the data.
+
+film_genre <- factor(c("Romance", "Comedy", "Drama", "Thriller", "Horror", "Mystery", "Musical", "Musical", "Crime", "Drama", "Romance"),
+                     levels = c("Romance", "Comedy", "Crime", "Drama", "Thriller", "Horror", "Mystery", "Musical", "Biopic", "Other"))
+
+film_genre # Note: if in the data there are items that are not a category/level, it is classed as <NA>.
+
+# To find out the number of items in a factor, use length().
+
+length(film_genre) # Should say 11
+
+# To access items in a factor use [].
+
+film_genre[3] # Shows the third data item
+
+film_genre[-1] # Shows all data items but the first one
+
+film_genre[1:3] # Shows first 3 data items
+
+film_genre[c(1,2,5)] # Shows 1st, 2nd and 5th data items
+
+film_genre[c(-1,-3)] # Shows all data items but the 1st and 3rd items.
