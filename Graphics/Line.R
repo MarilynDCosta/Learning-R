58541eca98ff52c88031e47986165a2a880b06b1
MarilynDCosta <125499347+MarilynDCosta@users.noreply.github.com>
2024-09-20 23:58:31 +0100

    Update Line.R

--- Graphics/Line.R
+++ Graphics/Line.R
@@ -1,12 +1,17 @@
-# To create line graphs, you can also use plot with type = "l"
+# To create line graphs, you can also use plot with type = "l".
 
 plot(1:20, type="l")
 
-# To change the colour of a line, use col
+# To change the colour of a line, use the col parameter.
 
 plot(1:20, type="l", col="red")
 
-# Line width can be adjusted by using lwd
+# Line width can be adjusted by using the lwd parameter.
 
 plot(1:20, type="l", col="red", lwd=2)
 
+# To change the style of a line, use the lty parameter.
+# The lty parameter must have a value between 0 and 6.
+# Each number corresponds to a different style.
+
+plot(1:20, type="l", col="red", lwd=2, lty=2)
