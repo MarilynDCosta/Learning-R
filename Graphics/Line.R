37ce9ec93dd9c7237d2f988cb64db5017605163a
MarilynDCosta <125499347+MarilynDCosta@users.noreply.github.com>
2024-09-20 23:53:03 +0100

    Add files via upload

---
+++ Graphics/Line.R
@@ -0,0 +1,12 @@
+# To create line graphs, you can also use plot with type = "l"
+
+plot(1:20, type="l")
+
+# To change the colour of a line, use col
+
+plot(1:20, type="l", col="red")
+
+# Line width can be adjusted by using lwd
+
+plot(1:20, type="l", col="red", lwd=2)
+
