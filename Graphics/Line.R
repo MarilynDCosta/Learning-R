45d19cc18868e8bfbb1c8c6a275a12e773e3d7bd
MarilynDCosta <125499347+MarilynDCosta@users.noreply.github.com>
2024-09-21 00:21:42 +0100

    Update Line.R

--- Graphics/Line.R
+++ Graphics/Line.R
@@ -1,4 +1,4 @@
-# To create line graphs, you can also use plot with type = "l".
+# To create line graphs, you can also use plot() with type = "l".
 
 plot(1:20, type="l")
 
@@ -15,3 +15,19 @@ plot(1:20, type="l", col="red", lwd=2)
 # Each number corresponds to a different style.
 
 plot(1:20, type="l", col="red", lwd=2, lty=2)
+
+# To display multiple lines in a graph, use the plot() function along with the lines() function.
+
+# To easily display lines that follow a mathematical formula, define your range of x-values and create variables for each line by setting them as the formula.
+
+x <- seq(-10, 10, by=0.1)
+
+y1 <- x
+y2 <- x^2
+y3 <- x^3
+
+plot(x,y1, type="l", col="red", lwd=2, xlab="x", ylab="y")
+
+lines(x, y2, type="l", col="blue", lwd=2, xlab="x", ylab="y")
+
+lines(x, y3, type="l", col="darkgreen", lwd=2, xlab="x", ylab="y")
